96026f966ce76705e847113acbfc5f2987bcd49e
yavuzakin <yavuzakin55@gmail.com>
2019-07-03 15:45:28 +0300

    Initial commit

---
+++ Palindrome Checker/main.js
@@ -0,0 +1,7 @@
+function palindrome(str) {
+
+    let newStr = [...str.toLowerCase().split(/[\s.,(_)-]/).join("")].toString();
+    let arr = [...str.toLowerCase().split(/[\s.,(_)-]/).join("")].reverse().toString();
+    return newStr === arr ? true : false;
+    
+}
