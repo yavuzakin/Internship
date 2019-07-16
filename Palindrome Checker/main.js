bfc92c0160d75dcc0a0a63558e6a569a355fff0d
yavuzakin <yavuzakin55@gmail.com>
2019-07-16 10:27:55 +0300

    Initial commit

--- Palindrome Checker/main.js
+++ Palindrome Checker/main.js
@@ -1,7 +1,6 @@
 function palindrome(str) {
-
     let newStr = [...str.toLowerCase().split(/[\s.,(_)-]/).join("")].toString();
     let arr = [...str.toLowerCase().split(/[\s.,(_)-]/).join("")].reverse().toString();
     return newStr === arr ? true : false;
-    
 }
+palindrome("eye");
