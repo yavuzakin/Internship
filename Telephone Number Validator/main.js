4ea298becfe44cbb95ed91c6160f0858656ab61a
yavuzakin <yavuzakin55@gmail.com>
2019-07-16 17:53:14 +0300

    Inıtial commit

---
+++ Telephone Number Validator/main.js
@@ -0,0 +1,16 @@
+function telephoneCheck(str) {
+    // Good luck!
+    
+    let strOne = str.split(/\D/).join("");
+    
+    if(str.match(/\w/)) return false;
+    if(str.split("(").length > 2) return false;
+    if(~str.indexOf(")") && !~str.indexOf("(") || (~str.indexOf("(") && str.split(/\(\d{3}\)/).length == 1)) return false;
+    if(!Number.isInteger(Number(str[0])) && str[0] !== "(") return false;
+    if(!(strOne.length == 10 || (strOne.length == 11 && str[0] == "1"))) return false;
+  
+    return true;
+    
+  }
+   
+  console.log(telephoneCheck("(605)4756j961"))
